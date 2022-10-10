0d4bc5ac0fb2abf5364a18b7f74cd2f25fea39ef
Samuel I. H. Sibuea <ifs20004@students.del.ac.id>
2022-10-10 13:31:33 +0700

    init 1

---
+++ proyekpabwe_kelompok2/src/App.js
@@ -0,0 +1,60 @@
+import react from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import logo_Del from './Assets/logo_Del.png';
+
+
+function App(){
+  return(
+    <div className="App">
+
+    </div>
+  );
+}
+// function App() {
+//   return (
+//     <>
+//     <header class="navbar navbar-expand bg-primary bg-gradient sticky-top head">
+//         <a class="navbar-brand mx-5 d-flex " href="#">
+//             <img src={logo_Del} alt="logo Del" width="60" height="60" class="d-inline-block  " />
+//             <p class="text-center my-2 px-4 text-light title">Sarjana Informatika</p>
+//         </a>
+//         <div class="m-auto" id="navbarNav">
+//               <ul class="navbar-nav  ">
+//                 <li class="nav-item list">
+//                   <a class="nav-link active text-light" aria-current="page" href="#">Beranda</a>
+//                 </li>
+//                 <li class="nav-item list">
+//                   <a class="nav-link text-light" href="#">Kegiatan</a>
+//                 </li>
+//                 <li class="nav-item list">
+//                   <a class="nav-link text-light" href="#">Prestasi</a>
+//                 </li>
+//                 <li class="nav-item list">
+//                   <a class="nav-link text-light" href='#'>Struktur Organisasi</a>
+//                 </li>
+//                 <li class="nav-item list">
+//                   <a class="nav-link text-light" href='#'>Tentang Kita</a>
+//                 </li>
+//                 <li class="nav-item list">
+//                   <a class="nav-link text-light" href='#'>Hubungi Kami</a>
+//                 </li>
+//               </ul>
+//         </div>
+//     </header>
+//     <div class="container content">
+//       <h1 class="text-center">
+//         TEST
+//       </h1>
+//       <div class= "container ">
+
+//       </div>
+//     </div>
+//     <footer></footer></>
+//   );
+// }
+
+
+
+
+export default App;
